5e944c1d97d53e807060eaa4edad22311adb8713
ABHISHEK TOMAR <103330372+Abhishek1044@users.noreply.github.com>
2022-07-09 22:26:39 +0530

    Update ques4.py

--- ques4.py
+++ ques4.py
@@ -1,7 +1,15 @@
 # pangram program
-string = input("Enter a sentence : \n \t")
-def pangram(string):
-    for i in string and j in range(chr(96), chr(122)):
-        if i ==j:
-            print("pangram")
-pangram(string)
+alphabets = "abcdefghijklmnopqrstuvwxyz"
+
+def ispangram(userstr):
+    for x in alphabets:
+        if x not in userstr.lower():
+            return False
+    return True
+
+input_str = str(input("Enter string to be checked if pangram or not : "))
+
+if(ispangram(input_str)==True):
+    print("Yes, it is a pangram.")
+else:
+    print("No, it is not a pangram.")
